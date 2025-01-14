58bbb9a0803a10321b0248e242a689ef83c7fd70
Cyprian Moj (cypekdev) <105518009+cypekdev@users.noreply.github.com>
2025-01-14 23:37:33 +0100

    Added a primitive console output content, added some data to visualize chart component

--- MeteoSat Stats/LineChart.xaml.cs
+++ MeteoSat Stats/LineChart.xaml.cs
@@ -12,6 +12,9 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using LiveCharts;
+using LiveCharts.Defaults;
+using LiveCharts.Wpf;
 
 namespace MeteoSat_Stats
 {
@@ -20,11 +23,51 @@ namespace MeteoSat_Stats
     /// </summary>
     public partial class LineChart : UserControl
     {
+        public SeriesCollection Series { get; set; }
         public LineChart()
         {
             InitializeComponent();
+            DataContext = this;
 
-
+            Series = new SeriesCollection
+            { 
+                new LineSeries
+                {
+                    Values = new ChartValues<ObservablePoint>
+                    {
+                        new ObservablePoint(0, 10),
+                        new ObservablePoint(4, 7),
+                        new ObservablePoint(5, 3),
+                        new ObservablePoint(7, 6),
+                        new ObservablePoint(10, 8)
+                    },
+                    PointGeometrySize = 15
+                },
+                new LineSeries
+                {
+                    Values = new ChartValues<ObservablePoint>
+                    {
+                        new ObservablePoint(0, 2),
+                        new ObservablePoint(2, 5),
+                        new ObservablePoint(3, 6),
+                        new ObservablePoint(6, 8),
+                        new ObservablePoint(10, 5)
+                    },
+                    PointGeometrySize = 15
+                },
+                new LineSeries
+                {
+                    Values = new ChartValues<ObservablePoint>
+                    {
+                        new ObservablePoint(0, 4),
+                        new ObservablePoint(5, 5),
+                        new ObservablePoint(7, 7),
+                        new ObservablePoint(9, 10),
+                        new ObservablePoint(10, 9)
+                    },
+                    PointGeometrySize = 15
+                }
+            };
         }
     }
 }
